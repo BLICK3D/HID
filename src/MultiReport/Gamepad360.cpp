4e03d5255520237cc6d3a46d1771d132167bb2c1
BLICK3D <kontakt@blick3d.com>
2017-05-29 00:33:05 +0200

    change xbox360 gamepad descriptor

--- src/MultiReport/Gamepad360.cpp
+++ src/MultiReport/Gamepad360.cpp
@@ -23,53 +23,102 @@ THE SOFTWARE.
 
 #include "Gamepad360.h"
 
+// descriptor data from https://gist.github.com/fendent/5709856 
 
 static const uint8_t _hidMultiReportDescriptorGamepad360[] PROGMEM = {
-	/* Gamepad with 32 buttons and 6 axis*/
-	0x05, 0x01,							/* USAGE_PAGE (Generic Desktop) */
-	0x09, 0x05,							/* USAGE (Gamepad) */
-	0xa1, 0x01,							/* COLLECTION (Application) */
-	0x85, HID_REPORTID_GAMEPAD,			/*   REPORT_ID */
-	/* 32 Buttons */
-	0x05, 0x09,							/*   USAGE_PAGE (Button) */
-	0x19, 0x01,							/*   USAGE_MINIMUM (Button 1) */
-	0x29, 0x20,							/*   USAGE_MAXIMUM (Button 32) */
-	0x15, 0x00,							/*   LOGICAL_MINIMUM (0) */
-	0x25, 0x01,							/*   LOGICAL_MAXIMUM (1) */
-	0x75, 0x01,							/*   REPORT_SIZE (1) */
-	0x95, 0x20,							/*   REPORT_COUNT (32) */
-	0x81, 0x02,							/*   INPUT (Data,Var,Abs) */
-	/* 4 16bit Axis */
-	0x05, 0x01,							/*   USAGE_PAGE (Generic Desktop) */
-	0xa1, 0x00,							/*   COLLECTION (Physical) */
-	0x09, 0x30,							/*     USAGE (X) */
-	0x09, 0x31,							/*     USAGE (Y) */
-	0x09, 0x33,							/*     USAGE (Rx) */
-	0x09, 0x34,							/*     USAGE (Ry) */
-	0x16, 0x00, 0x80,					/*     LOGICAL_MINIMUM (-32768) */
-	0x26, 0xFF, 0x7F,					/*     LOGICAL_MAXIMUM (32767) */
-	0x75, 0x10,							/*     REPORT_SIZE (16) */
-	0x95, 0x04,							/*     REPORT_COUNT (4) */
-	0x81, 0x02,							/*     INPUT (Data,Var,Abs) */
-	/* 2 8bit Axis */
-	0x09, 0x32,							/*     USAGE (Z) */
-	0x09, 0x35,							/*     USAGE (Rz) */
-	0x15, 0x80,							/*     LOGICAL_MINIMUM (-128) */
-	0x25, 0x7F,							/*     LOGICAL_MAXIMUM (127) */
-	0x75, 0x08,							/*     REPORT_SIZE (8) */
-	0x95, 0x02,							/*     REPORT_COUNT (2) */
-	0x81, 0x02,							/*     INPUT (Data,Var,Abs) */
-	0xc0,								/*   END_COLLECTION */
-	/* 2 Hat Switches */
-	0x05, 0x01,							/*   USAGE_PAGE (Generic Desktop) */
-	0x09, 0x39,							/*   USAGE (Hat switch) */
-	0x09, 0x39,							/*   USAGE (Hat switch) */
-	0x15, 0x01,							/*   LOGICAL_MINIMUM (1) */
-	0x25, 0x08,							/*   LOGICAL_MAXIMUM (8) */
-	0x95, 0x02,							/*   REPORT_COUNT (2) */
-	0x75, 0x04,							/*   REPORT_SIZE (4) */
-	0x81, 0x02,							/*   INPUT (Data,Var,Abs) */
-	0xc0								/* END_COLLECTION */
+    0x05, 0x01,                    // USAGE_PAGE (Generic Desktop)
+    0x09, 0x05,                    // USAGE (Game Pad)
+    0xa1, 0x01,                    // COLLECTION (Application)
+    0x05, 0x01,                    //   USAGE_PAGE (Generic Desktop)
+    0x09, 0x3a,                    //   USAGE (Counted Buffer)
+    0xa1, 0x02,                    //   COLLECTION (Logical)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x3f,                    //     USAGE (Reserved)
+    0x09, 0x3b,                    //     USAGE (Byte Count)
+    0x81, 0x01,                    //     INPUT (Cnst,Ary,Abs)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x35, 0x00,                    //     PHYSICAL_MINIMUM (0)
+    0x45, 0x01,                    //     PHYSICAL_MAXIMUM (1)
+    0x95, 0x04,                    //     REPORT_COUNT (4)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x0c,                    //     USAGE_MINIMUM (Button 12)
+    0x29, 0x0f,                    //     USAGE_MAXIMUM (Button 15)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x35, 0x00,                    //     PHYSICAL_MINIMUM (0)
+    0x45, 0x01,                    //     PHYSICAL_MAXIMUM (1)
+    0x95, 0x04,                    //     REPORT_COUNT (4)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x09, 0x09,                    //     USAGE (Button 9)
+    0x09, 0x0a,                    //     USAGE (Button 10)
+    0x09, 0x07,                    //     USAGE (Button 7)
+    0x09, 0x08,                    //     USAGE (Button 8)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x35, 0x00,                    //     PHYSICAL_MINIMUM (0)
+    0x45, 0x01,                    //     PHYSICAL_MAXIMUM (1)
+    0x95, 0x03,                    //     REPORT_COUNT (3)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x09, 0x05,                    //     USAGE (Button 5)
+    0x09, 0x06,                    //     USAGE (Button 6)
+    0x09, 0x0b,                    //     USAGE (Button 11)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x95, 0x01,                    //     REPORT_COUNT (1)
+    0x81, 0x01,                    //     INPUT (Cnst,Ary,Abs)
+    0x75, 0x01,                    //     REPORT_SIZE (1)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x25, 0x01,                    //     LOGICAL_MAXIMUM (1)
+    0x35, 0x00,                    //     PHYSICAL_MINIMUM (0)
+    0x45, 0x01,                    //     PHYSICAL_MAXIMUM (1)
+    0x95, 0x04,                    //     REPORT_COUNT (4)
+    0x05, 0x09,                    //     USAGE_PAGE (Button)
+    0x19, 0x01,                    //     USAGE_MINIMUM (Button 1)
+    0x29, 0x04,                    //     USAGE_MAXIMUM (Button 4)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x75, 0x08,                    //     REPORT_SIZE (8)
+    0x15, 0x00,                    //     LOGICAL_MINIMUM (0)
+    0x26, 0xff, 0x00,              //     LOGICAL_MAXIMUM (255)
+    0x35, 0x00,                    //     PHYSICAL_MINIMUM (0)
+    0x46, 0xff, 0x00,              //     PHYSICAL_MAXIMUM (255)
+    0x95, 0x02,                    //     REPORT_COUNT (2)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x32,                    //     USAGE (Z)
+    0x09, 0x35,                    //     USAGE (Rz)
+    0x81, 0x02,                    //     INPUT (Data,Var,Abs)
+    0x75, 0x10,                    //     REPORT_SIZE (16)
+    0x16, 0x00, 0x80,              //     LOGICAL_MINIMUM (-32768)
+    0x26, 0xff, 0x7f,              //     LOGICAL_MAXIMUM (32767)
+    0x36, 0x00, 0x80,              //     PHYSICAL_MINIMUM (-32768)
+    0x46, 0xff, 0x7f,              //     PHYSICAL_MAXIMUM (32767)
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x01,                    //     USAGE (Pointer)
+    0xa1, 0x00,                    //     COLLECTION (Physical)
+    0x95, 0x02,                    //       REPORT_COUNT (2)
+    0x05, 0x01,                    //       USAGE_PAGE (Generic Desktop)
+    0x09, 0x30,                    //       USAGE (X)
+    0x09, 0x31,                    //       USAGE (Y)
+    0x81, 0x02,                    //       INPUT (Data,Var,Abs)
+    0xc0,                          //     END_COLLECTION
+    0x05, 0x01,                    //     USAGE_PAGE (Generic Desktop)
+    0x09, 0x01,                    //     USAGE (Pointer)
+    0xa1, 0x00,                    //     COLLECTION (Physical)
+    0x95, 0x02,                    //       REPORT_COUNT (2)
+    0x05, 0x01,                    //       USAGE_PAGE (Generic Desktop)
+    0x09, 0x33,                    //       USAGE (Rx)
+    0x09, 0x34,                    //       USAGE (Ry)
+    0x81, 0x02,                    //       INPUT (Data,Var,Abs)
+    0xc0,                          //     END_COLLECTION
+    0xc0,                          //   END_COLLECTION
+    0xc0                           // END_COLLECTION
 };
 
 Gamepad360_::Gamepad360_(void) 
